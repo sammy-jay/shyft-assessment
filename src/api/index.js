703471d192bbef34abc42c29ce71f3c68552af2a
sammy-jay <soneyetolulope4@gmail.com>
2022-05-26 15:43:06 +0100

    updated api folder

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const API = axios.create({ baseURL: "https://api-dev.trysolstice.com/v1" });
 
 API.interceptors.request.use((req) => {
-  req.headers.Authorization = `Bearer ${process.env.AUTH_TOKEN}`;
+  // req.headers.Authorization = `Bearer ${process.env.AUTH_TOKEN}`;
+  req.headers.Authorization = "Bearer zUKWzuo6UBFT-nu4HVmk";
 
   return req;
 });
