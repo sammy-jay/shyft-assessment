992da28e2695b72c7f4ec35f889e5c6b78213425
sammy-jay <soneyetolulope4@gmail.com>
2022-06-01 20:17:04 +0100

    minor changes

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://api-dev.trysolstice.com/v1" });
+const API = axios.create({
+  baseURL: "https://api-dev.trysolstice.com/v1",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  },
+});
 
 API.interceptors.request.use((req) => {
   req.headers.Authorization = "Bearer zUKWzuo6UBFT-nu4HVmk";
